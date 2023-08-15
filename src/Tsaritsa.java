b81ea64374a88367ba0281e90befb95ba0fae9f3
YungChepr <chandy.ru@gmail.com>
2023-08-15 09:53:28 +0700

    Рабочий прототип с бегающими мураьвями которые имеют жизненную cилу и едят еду

--- src/Tsaritsa.java
+++ src/Tsaritsa.java
@@ -2,4 +2,6 @@ public class Tsaritsa extends Muravei{
     public Tsaritsa(int koordinX, int koordinY){
         super(koordinX,koordinY);
     }
+    //Царица существует всегда пока есть муравьи, и спамит новых муравьёв
+    boolean shizn;
 }
